f1432c749edc4aea33b29c0be2f2b16f57d2d098
codyhusek <cody.husek@husoftsolutions.com>
2023-11-24 00:07:52 -0500

    fixed firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,4 @@
-import { initializeApp, firebase } from "firebase/app";
+import firebase, { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getFunctions, httpsCallable } from "@firebase/functions";
